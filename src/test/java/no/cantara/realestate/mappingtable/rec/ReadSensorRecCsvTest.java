935649427fa141ef759bd6252f625a6ed76e07bc
baardl <bard.lind@gmail.com>
2023-04-17 11:32:28 +0200

    #23 Parse RecSensor csv file

---
+++ src/test/java/no/cantara/realestate/mappingtable/rec/ReadSensorRecCsvTest.java
@@ -0,0 +1,25 @@
+package no.cantara.realestate.mappingtable.rec;
+
+import no.cantara.realestate.mappingtable.csv.CsvCollection;
+import no.cantara.realestate.mappingtable.csv.CsvReader;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReadSensorRecCsvTest {
+
+    @Test
+    void readBasic() {
+        CsvCollection collection = CsvReader.parse("src/test/resources/RecSensor.csv");
+        assertNotNull(collection);
+        assertTrue(collection.getColumnNames().contains("RecId"));
+        assertTrue(collection.getColumnNames().contains("RealEstate"));
+        assertTrue(collection.getColumnNames().contains("Section"));
+        Map record = collection.getRecords().get(0);
+        assertEquals("aaa-bbb-ccc", record.get("RecId"));
+        assertEquals("re16", record.get("RealEstate"));
+        assertEquals("",record.get("Section"));
+    }
+}
