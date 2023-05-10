1b41d962e05af6621621d633f2bfcba09de33d3a
baardl <bard.lind@gmail.com>
2023-05-10 14:38:44 +0200

    Simple load for Metasys

--- src/main/java/no/cantara/realestate/mappingtable/Main.java
+++ src/main/java/no/cantara/realestate/mappingtable/Main.java
@@ -6,14 +6,43 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.util.StdDateFormat;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import no.cantara.config.ApplicationProperties;
+import no.cantara.realestate.mappingtable.importer.CsvSensorImporter;
+import no.cantara.realestate.mappingtable.metasys.MetasysCsvSensorImporter;
+import no.cantara.realestate.mappingtable.repository.MappedIdRepository;
+import no.cantara.realestate.mappingtable.repository.MappedIdRepositoryImpl;
+import org.slf4j.Logger;
+
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Hello world!
  */
 public class Main {
+    private static final Logger log = getLogger(Main.class);
+
+    private MappedIdRepository mappedIdRepository;
 
     boolean loadPlugins() {
-        return true;
+        boolean loadedOk = false;
+        try {
+            String configDirectory = "import-data";
+            if (!Paths.get(configDirectory).toFile().exists()) {
+                throw new MappingTableException("Import of data from " + configDirectory + " failed. Directory does not exist.");
+            }
+            CsvSensorImporter csvSensorImporter = new MetasysCsvSensorImporter(Paths.get(configDirectory).toFile());
+            List<MappedSensorId> mappedSensorIds = csvSensorImporter.importMappedId("metasys");
+            for (MappedSensorId mappedSensorId : mappedSensorIds) {
+                mappedIdRepository.add(mappedSensorId);
+            }
+            loadedOk = true;
+        } catch (Exception e) {
+            MappingTableException me = new MappingTableException("Failed to load plugins",e);
+            log.error("Failure loading plugins. {}", me.getMessage(), e);
+        }
+        return loadedOk;
     }
 
     public static ObjectMapper initializeObjectMapper() {
@@ -35,11 +64,21 @@ public class Main {
     }
     public static void main(String[] args) {
         Main main = new Main();
+        main.initialize();
         if (main.loadPlugins()) {
-            System.out.println("Plugins loaded ok");
+            log.info("Plugins loaded ok.");
         } else {
-            System.err.println("Failed to load plugins");
+            log.error("Failed to load plugins");
         }
+        log.info("Loaded {} sensors.", main.getMappedIdRepository().size());
+    }
+
+    private MappedIdRepository getMappedIdRepository() {
+        return mappedIdRepository;
+    }
+
+    void initialize() {
+        mappedIdRepository = new MappedIdRepositoryImpl();
     }
 
 }
